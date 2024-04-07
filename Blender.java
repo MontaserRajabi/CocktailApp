9eb9600cec974598f92b2ac1b3a5f9f491bfaac3
MontaserRajabi <165844000+MontaserRajabi@users.noreply.github.com>
2024-04-07 16:32:24 +0200

    Blender.java

--- Blender.java
+++ Blender.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package cocktailapp;
 
 import java.awt.Color;
 import java.util.ArrayList;
 
-/**
- *
- * @author MontaserPC
- */
 public class Blender {
-   private ArrayList<Ingredients> ingred = new ArrayList<>();
+
+    private ArrayList<Ingredients> ingred = new ArrayList<>();
     private int capacity;
     private int totalCalories;
     private int volume;
@@ -22,31 +16,39 @@ public class Blender {
         this.capacity = capacity;
         this.totalCalories = 0;
         this.volume = 0;
-       
     }
+
     public static Color mixColors(Color color1, Color color2) {
         int red = (color1.getRed() + color2.getRed()) / 2;
         int green = (color1.getGreen() + color2.getGreen()) / 2;
         int blue = (color1.getBlue() + color2.getBlue()) / 2;
-        
+
         return new Color(red, green, blue);
     }
-     public void blend() {
+
+    public void blend() {
         System.out.println("Blending ingredients...");
     }
-     public int totalCalories() {
+
+    public int totalCalories() {
         int total = 0;
         for (Ingredients ing : ingred) {
             total += ing.getCalories();
         }
         return total;
     }
-     public double totalVolume() {
+
+    public double totalVolume() {
         double total = 0;
         for (Ingredients ing : ingred) {
+            total += ing.getVolume();
             if (ing instanceof Milk) {
                 total += ((Milk) ing).getVolume();
             }
+            if (ing instanceof Suger) {
+                total += ((Suger) ing).getVolume();
+            }
+
         }
         return total;
     }
@@ -59,16 +61,20 @@ public class Blender {
         return volume;
     }
 
-  public void addIngredient(Ingredients ing) {
-       if (totalVolume() + ing.getVolume() <= capacity) {
+    public void addIngredient(Ingredients ing) {
+        if (totalVolume() + ing.getVolume() <= capacity) {
             ingred.add(ing);
         } else {
-            System.out.println("Blender capacity exceeded!");
+            System.out.println("Blender Capacity Full!");
         }
 
         totalCalories += ing.getCalories();
         volume += ing.getVolume();
         Color mixedColor = mixColors(color, ing.getColor());
     }
-      
+
+    public String getInfo() {
+        return "Blender{" + "ingred=" + ingred + ", capacity=" + capacity + ", totalCalories=" + totalCalories + ", volume=" + volume + ", color=" + color.toString() + '}';
+    }
+
 }
