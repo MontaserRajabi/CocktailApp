d23640ab795edc57358d7029c802e5acbbb3037b
MontaserRajabi <165844000+MontaserRajabi@users.noreply.github.com>
2024-04-27 23:40:31 +0300

    Milk.java

--- Milk.java
+++ Milk.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package cocktailapp;
 
-/**
- *
- * @author MontaserPC
- */
 public class Milk extends Ingredients{  
 
     public Milk(String name, double calories, double volume ) {
