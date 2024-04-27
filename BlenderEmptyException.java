cc4c200b3fb69ae03da5c2fbd6f4ac65c48c3f20
MontaserRajabi <165844000+MontaserRajabi@users.noreply.github.com>
2024-04-27 22:27:19 +0200

    Add files via upload

---
+++ BlenderEmptyException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cocktailapp;
+
+/**
+ *
+ * @author MontaserPC
+ */
+public class BlenderEmptyException extends Exception {
+    public BlenderEmptyException() {
+        super("The blender is empty!");
+    }
+}
