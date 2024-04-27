9c911e315c503157bb85048f8f60631ac1ffd9d1
MontaserRajabi <165844000+MontaserRajabi@users.noreply.github.com>
2024-04-27 22:28:11 +0200

    Add files via upload

---
+++ BlenderOverFlowException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cocktailapp;
+
+/**
+ *
+ * @author MontaserPC
+ */
+public class BlenderOverFlowException extends Exception {
+     public BlenderOverFlowException(){
+        super("Ingredients volume exceeds blender capacity");
+    }
+}
